1c397b0f04cc3f92cc8159db84f6e2589ecf1594
Nataset Tanabodee <nataset.t@ku.th>
2022-04-21 23:55:45 +0700

    add ar-product info

--- src/page/ar/index.js
+++ src/page/ar/index.js
@@ -5,41 +5,118 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import setFavicon from '../../utitly/setFavicon';
 import setMetaTag from '../../utitly/setMetaTags';
-
-import ar_code_img1 from '../../assets/ar/cleansingGel-ar.png';
-import ar_code_img2 from '../../assets/ar/cornMilk-ar.png';
-import ar_code_img3 from '../../assets/ar/kuMilk-ar.png';
-import ar_code_img4 from '../../assets/ar/ciderVinegar-ar.png';
-
-import product_img1 from '../../assets/ar/cleansingGel.png';
-import product_img2 from '../../assets/ar/cornMilk.png';
-import product_img3 from '../../assets/ar/kuMilk.png';
-import product_img4 from '../../assets/ar/ciderVinegar.jpg';
-
 import phone_model from '../../assets/model/phone.gltf';
-
 import icon_img from '../../assets/favicon.ico';
+import qrCode_img from '../../assets/ar/QRCode.png';
+const arImages = importAll(require.context('../../assets/ar/', false, /\.(png|jpe?g)$/));
 
-function setImgSrc() {
-    const ar1 = document.getElementById('ar_code1');
-    const ar2 = document.getElementById('ar_code2');
-    const ar3 = document.getElementById('ar_code3');
-    const ar4 = document.getElementById('ar_code4');
+const arContents = [
+    {
+        title: 'Ku Milk นมเกษตร',
+        descrip: `“นมเกษตร” เป็นได้มากกว่านม"<br>
+        “นมเกษตร” หรือ KU milk ดำเนินการผลิตโดยมหาวิทยาลัยเกษตรศาสตร์ เป็นผลิตภัณฑ์นมแห่งแรกๆของประเทศไทย ที่ผลิตโดยภาครัฐ ที่คู่ขนานไปกับการเรียนการสอนมาเป็นระยะเวลาอันยาวนานเกินกว่าครึ่งศตวรรษ มาจนถึง ณ วันนี้ เกษตรศาสตร์กำลังจะขับเคลื่อน “ นมเกษตร ให้เป็นได้มากกว่านมและอาหารของคนไทย”<br><span style="font-weight: 600">--- บาท</span>`,
+        price: null,
+        arImg: arImages[`kuMilk-ar.png`],
+        productImg: arImages[`kuMilk.png`],
+    },
+    {
+        title: 'น้ำนมข้าวโพด หวาน 97.8% ไร่สุวรรณ',
+        descrip: `น้ำนมข้าวโพดแท้ ส่งตรงมาจากไร่สุวรรณ ดื่นง่าย หวานชื่นใจ เครื่องดื่มสุดอร่อย อุดมด้วยคุณค่าทางโภชนาการ ผลิตจำหน่ายสดใหม่วันต่อวัน โดยศูนย์วิจัยข้าวโพดและข้าวฟ่างแห่งชาติ มหาวิทยาลัยเกษตรศาสตร์ จังหวัดนครราชสีมา <br><span style="font-weight: 600">25 บาท</span>`,
+        price: 25,
+        arImg: arImages[`cornMilk-ar.png`],
+        productImg: arImages[`cornMilk.png`],
+    },
+    {
+        title: 'ชากัญชา KU Cannafe',
+        descrip: `เครื่องน้ำชาผสมกัญชา เป็นหนึ่งในผลงานวิจัยจากคณะเกษตร มหาวิทยาลัยเกษตรศาสตร์<br><span style="font-weight: 600">--- บาท</span>`,
+        price: null,
+        arImg: arImages[`canabisTea-ar.png`],
+        productImg: arImages[`canabisTea.png`],
+    },
+    {
+        title: 'เนื้อวากิว มหาลัยเกษตร',
+        descrip: `เนื้อวากิวเกรดพรีเมียม สามารถนำไปประกอบเป็นอาหารได้หลายอย่าง ผลิตภัณฑ์จากมหาวิทยาลัยเกษตรศาสตร์ วิทยาเขตกำแพงแสน<br><span style="font-weight: 600">--- บาท</span>`,
+        price: null,
+        arImg: arImages[`wagyuBeef-ar.png`],
+        productImg: arImages[`wagyuBeef.png`],
+    },
+    {
+        title: 'KAPIOKU Cleansing Water',
+        descrip: `สำหรับผิวแพ้ง่ายมาก กลิ่นหอมสดชื่น จากใบเสม็ดขาว ไม่มีส่วนผสมจากน้ำหอม ไม่มีสารพาราเบน <br><span style="font-weight: 600">250 บาท</span>`,
+        price: 250,
+        arImg: arImages[`cleansingWater-ar.png`],
+        productImg: arImages[`cleansingWater.png`],
+    },
 
-    const product1 = document.getElementById('product1');
-    const product2 = document.getElementById('product2');
-    const product3 = document.getElementById('product3');
-    const product4 = document.getElementById('product4');
+    {
+        title: 'KAPIOKU Cleansing Gel',
+        descrip: `เจลทำความสะอาดผิวหน้าสูตรอ่อนโยน ที่เหมาะกับทุกสภาพผิว ช่วยทำความสะอาดผิวได้หมดจดจริงๆไม่ว่าจะเป็น ฝุ่น มลภาวะ ส่วนใครที่มีปัญหาสิว หรือมีแนวโน้มที่เป็นสิวง่ายก็ยิ่งตอบโจทย์เนื่องจากมีส่วนผสมของน้ำมันหอมระเหยเสม็ดขาว และสารสกัดจากชาเขียวออร์แกนิค<br><span style="font-weight: 600">150 บาท</span>`,
+        price: 150,
+        arImg: arImages[`cleansingGel-ar.png`],
+        productImg: arImages[`cleansingGel.png`],
+    },
+    {
+        title: 'KAPIOKU Anti Acne',
+        descrip: `เซรั่มลดสิว สบายใจเรื่องการระคายเคืองไปเปราะนึงเลย เพราะอะไรใช่มั๊ย ไม่มีสารก่อสิว ไม่มีพาราเบน และที่ปลื้มสุดๆอะ คือไม่มี แอลกอฮอล์<br><span style="font-weight: 600">390 บาท</span>`,
+        price: 390,
+        arImg: arImages[`kapiAntiAcne-ar.png`],
+        productImg: arImages[`kapiAntiAcne.png`],
+    },
+    {
+        title: 'Fruit Vinegar Drink น้ำส้มสายชูผสมน้ำผลไม้',
+        descrip: `เครื่องดื่มหมักจากธรรมชาติ มีคุณสมบัติช่วยลดคอเลสเตอรอล ช่วยลดไตรกลีเซอร์ไรด์และไขมันชนิด low-density lipoprotein (LDL) มีสารต้านอนุมูลอิสระ ช่วยชะลอวัย และป้องกันโรคมะเร็ง ช่วยย่อยอาหาร ไม่แต่งกลิ่น ไม่เจือสีสังเคราะห์ ไม่ใช้วัตถุกันเสีย ดื่มง่าย<br><span style="font-weight: 600">--- บาท</span>`,
+        price: null,
+        arImg: arImages[`ciderVinegar-ar.png`],
+        productImg: arImages[`ciderVinegar.jpg`],
+    },
+];
 
-    ar1.src = ar_code_img1;
-    ar2.src = ar_code_img2;
-    ar3.src = ar_code_img3;
-    ar4.src = ar_code_img4;
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+        images[item.replace('./', '')] = r(item).default;
+    });
+    return images;
+}
 
-    product1.src = product_img1;
-    product2.src = product_img2;
-    product3.src = product_img3;
-    product4.src = product_img4;
+function addInfoElement(arContents) {
+    const mainDiv = document.querySelector('.more-info');
+    arContents.forEach(product => {
+        let info = document.createElement('div');
+        let content = document.createElement('div');
+        let h3 = document.createElement('h3');
+        let p = document.createElement('p');
+        let imgDiv = document.createElement('div');
+        let arImg = document.createElement('img');
+        let productImg = document.createElement('img');
+
+        info.className = 'more-info__feature';
+        content.className = 'more-info__content';
+        h3.className = 'more-info__title';
+        p.className = 'more-info__descrip';
+        imgDiv.className = 'more-info__img';
+        arImg.className = 'more-info__ar-code';
+        productImg.className = 'more-info__product';
+
+        h3.innerText = product.title;
+        p.innerHTML = product.descrip;
+        arImg.src = product.arImg;
+        productImg.src = product.productImg;
+
+        info.appendChild(content);
+        content.appendChild(h3);
+        content.appendChild(p);
+        info.appendChild(imgDiv);
+        imgDiv.appendChild(arImg);
+        imgDiv.appendChild(productImg);
+
+        mainDiv.appendChild(info);
+    });
+}
+
+function loadQRCodeImg() {
+    const img = document.getElementById('qr-code');
+    img.src = qrCode_img;
 }
 
 function ar() {
@@ -104,5 +181,6 @@ function ar() {
 
 setMetaTag();
 setFavicon(icon_img);
-setImgSrc();
+loadQRCodeImg();
+addInfoElement(arContents);
 ar();
