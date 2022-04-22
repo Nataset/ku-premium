848ce1ea01e1d295d8c73739524b3e072b2ee288
Nataset Tanabodee <nataset.t@ku.th>
2022-04-22 22:55:34 +0700

    finish foodTrace Page

--- src/page/foodTrace/index.js
+++ src/page/foodTrace/index.js
@@ -1,1 +1,23 @@
-document.querySelector('.test').innerHTML = '<h3>TEST</h3>';
+import './index.css';
+const images = importAll(require.context('../../assets/foodTrace/', false, /\.(png|jpe?g)$/));
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+        images[item.replace('./', '')] = r(item).default;
+    });
+    return images;
+}
+
+document.getElementById('step1_img').src = images['step1.png'];
+document.getElementById('step1-qr_img').src = images['step1-qr.png'];
+document.getElementById('step2_img').src = images['step2.png'];
+document.getElementById('step2-qr_img').src = images['step2-qr.png'];
+document.getElementById('step3_img').src = images['step3.png'];
+document.getElementById('step3-qr_img').src = images['step3-qr.png'];
+document.getElementById('step4_img').src = images['step4.png'];
+document.getElementById('step4-qr_img').src = images['step4-qr.png'];
+document.getElementById('step5_img').src = images['step5.png'];
+document.getElementById('step5-qr_img').src = images['step5-qr.png'];
+document.getElementById('step6_img').src = images['step6.png'];
+document.getElementById('step6-qr_img').src = images['step6-qr.png'];
